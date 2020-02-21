3d978c860ed4a10fb1f9445972050e04288780c0
Syed Ali Raza <46275686+syedalirazarizvi@users.noreply.github.com>
2020-02-22 01:24:06 +0500

    knowing conversion value of bitcoin with USD
    # knowing conversion value of bitcoin with USD
    # in this script we have used request module
    # you need to install requests module using pip install requests

---
+++ bitcoinrates.py
@@ -0,0 +1,16 @@
+# knowing conversion value of bitcoin with PKR
+# in this script we have used request module
+# you need to install requests module using pip install requests
+
+import requests
+res = requests.get("https://api.livecoin.net/exchange/ticker?currencyPair=BTC/USD")
+
+print(res)
+price = res.json()
+
+pr = price["last"]
+
+print('Last Rates of Bitcoin {0:.0f} m'.format(pr))
+
+
+                   
