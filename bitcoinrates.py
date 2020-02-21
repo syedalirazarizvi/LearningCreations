fa4d8eefaa3c331b9b41c5068a95dc0ca2fb684b
Syed Ali Raza <46275686+syedalirazarizvi@users.noreply.github.com>
2020-02-22 01:26:49 +0500

    Update bitcoinrates.py

--- bitcoinrates.py
+++ bitcoinrates.py
@@ -1,11 +1,10 @@
-# knowing conversion value of bitcoin with PKR
+# knowing conversion value of bitcoin with USD
 # in this script we have used request module
 # you need to install requests module using pip install requests
 
 import requests
 res = requests.get("https://api.livecoin.net/exchange/ticker?currencyPair=BTC/USD")
 
-print(res)
 price = res.json()
 
 pr = price["last"]
